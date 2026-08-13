b5a694cd3b96612d73fb8c70f1d9f9340efe5fc0
Chefrel Baracol <chefrelbarac@gmail.com>
2026-08-13 09:11:51 +0800

    consider consumables tracking too

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 const PREFIX = 'pos-cache';
-const CACHE_NAME = PREFIX + '-v3.0.0';
+const CACHE_NAME = PREFIX + '-v3.0.1';
 
 // Optional: pre-cache a few known static assets on install.
 // Not required for SWR to work — it'll cache things on first fetch anyway.
