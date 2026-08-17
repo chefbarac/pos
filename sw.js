0c40ff42597f6fe4fc23ae2a732f5e58570a5bb2
Chefrel Baracol <chefrelbarac@gmail.com>
2026-08-17 17:26:15 +0800

    added sync

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 const PREFIX = 'pos-cache';
-const CACHE_NAME = PREFIX + '-v3.2.7';
+const CACHE_NAME = PREFIX + '-v3.3.0';
 
 // Optional: pre-cache a few known static assets on install.
 // Not required for SWR to work — it'll cache things on first fetch anyway.
