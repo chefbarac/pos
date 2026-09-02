7d450ce65c58bbb7447b73c602629913eca79d79
Chefrel Baracol <chefrelbarac@gmail.com>
2026-09-02 12:26:39 +0800

    added keywords for include options: "with options", "with eyelet"

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 const PREFIX = 'pos-cache';
-const CACHE_NAME = PREFIX + '-v3.4.11';
+const CACHE_NAME = PREFIX + '-v3.4.12';
 
 // Optional: pre-cache a few known static assets on install.
 // Not required for SWR to work — it'll cache things on first fetch anyway.
